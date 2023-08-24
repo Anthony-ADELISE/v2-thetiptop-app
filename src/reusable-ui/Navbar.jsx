6a99d9fbc8789a5b4aed6d9705637a808ce39116
Anthony-ADELISE <anthony.adelise@gmail.com>
2023-08-24 13:47:06 +0200

    Navbar > Add Navbar mobile

--- src/reusable-ui/Navbar.jsx
+++ src/reusable-ui/Navbar.jsx
@@ -12,6 +12,7 @@ const Navbar = () => {
   };
   return (
     <header>
+      {/* nav web */}
       <nav className="px-3 lg:px-12 pt-14 flex justify-between w-full items-center font-bold text-white text-base max-w-[1560px] mx-auto">
         {/* left navbar */}
         <div className="hidden lg:flex items-center gap-x-14">
@@ -35,6 +36,32 @@ const Navbar = () => {
           </button>
         </div>
       </nav>
+      {/* nav mobile */}
+      {isOpen && (
+        <nav className="lg:hidden fixed rounded-t-xl bottom-0 h-2/3 px-3 lg:px-12 pt-14 flex flex-col justify-around w-full items-center font-semibold bg-backgroundTip text-greenTip text-xl max-w-[1560px] mx-auto">
+          {/* left navbar */}
+          <div className="flex flex-col lg:hidden items-center space-y-16">
+            <Link href="/">Home</Link>
+            <Link href="/">Jeux concours</Link>
+            <Link href="/">Mon compte</Link>
+          </div>
+          {/* logo */}
+          <div className="hidden">
+            <Logo />
+          </div>
+          {/* right SideBar */}
+          <div className="flex flex-col lg:hidden items-center gap-y-10 text-base font-bold">
+            <div className="flex gap-x-8">
+              <a href="">IG</a>
+              <a href="">FB</a>
+              <a href="">Lkn</a>
+            </div>
+            <button className="bg-greenTip py-5 px-9 rounded-full text-backgroundTip hover:text-white">
+              Se connecter
+            </button>
+          </div>
+        </nav>
+      )}
 
       <button
         className=" flex-col justify-center items-center lg:hidden absolute right-10 z-30 top-20"
