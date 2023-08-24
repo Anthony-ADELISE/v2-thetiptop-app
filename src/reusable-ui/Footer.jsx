ede81a55ddea9a2ed02d232c7e137a02ac71a398
Anthony-ADELISE <anthony.adelise@gmail.com>
2023-08-24 15:16:23 +0200

    Footer > Add style

--- src/reusable-ui/Footer.jsx
+++ src/reusable-ui/Footer.jsx
@@ -1,7 +1,15 @@
 import React from "react";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <footer>
+      <ul className="flex flex-col justify-center items-center gap-y-5 text-white pb-7 font-semibold">
+        <li>Confidentialité</li>
+        <li>CGU</li>
+        <li>© Copyright 2023. All Right Reserved.</li>
+      </ul>
+    </footer>
+  );
 };
 
 export default Footer;
